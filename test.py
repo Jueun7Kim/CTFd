307c5e0f1c43797cefde4e583dcccc297bc77e46
Jueun <jueun@Jueunui-MacBookPro.local>
2023-10-11 22:59:09 +0900

    add stg

--- test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-command = 'ping -c 4 0.0.1'
-
-try:
-    result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
-    print(result)
-except subprocess.CalledProcessError as e:
-    print("오류 발생 : ", e)
